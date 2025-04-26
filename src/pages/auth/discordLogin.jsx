bedb13e469f82ca18c0423c40dc5a679cd509150
MateTheDev <szentpeteri.mate1166@gmail.com>
2025-04-26 09:50:28 +0200

    Added _redirects file

--- src/pages/auth/discordLogin.jsx
+++ src/pages/auth/discordLogin.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 function DiscordLogin() {
   useEffect(() => {
-    const url = "https://discord.com/oauth2/authorize?client_id=1365412353067450489&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fauth%2Fdiscord%2Fcallback&scope=identify"
+    const url = "https://discord.com/oauth2/authorize?client_id=1365412353067450489&response_type=code&redirect_uri=https%3A%2F%2Fmatethedev.com%2Fauth%2Fdiscord%2Fcallback&scope=identify"
     window.location.href = url;
   });
 
